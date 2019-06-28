95bf347edd9cd64c23531140a56a13c91d6adaa0
AndreBtt <bittencourtandre@hotmail.com>
2019-06-28 19:42:26 -0300

    added all instructions

--- exp/Teste.java
+++ exp/Teste.java
@@ -1,7 +1,9 @@
 class Teste {
-
 	public static void main(String[] args) {
-        int ola = 15;
-        System.out.println(ola);
+        outra();
+    }
+    
+    public static void outra() {
+        System.out.println("eita");
     }
 }
